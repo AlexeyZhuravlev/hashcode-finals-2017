57342f1b84be2bb02de8887b020d780b85e69f9f
Alsarmini Kinan Mamunovich <kinan.sarmini@gmail.com>
2017-04-01 12:28:18 +0300

    input + partial validation

--- kinan.cpp
+++ kinan.cpp
@@ -10,6 +10,7 @@
 #include <set>
 #include <map>
 #include <cstdlib>
+#include <cassert>
 
 using namespace std;
 
@@ -28,11 +29,40 @@ typedef long long ll;
 #define forn(i, n) for (int i = 0; i < (int)n; i++)
 #define fore(i, b, e) for (int i = (int)b; i <= (int)e; i++)
 
-int a, b, c;
+const int MAX_N = 10005;
+
+int h, w, r;
+int price_b, price_r, budget;
+pii initial_backbone;
+string layout[MAX_N];
+
+vector<pii> backbone_ans, router_ans;
 
 void read_input()
 {
-    cin >> a >> b;
+    cin >> h >> w >> r;
+    cin >> price_b >> price_r >> budget;
+    cin >> initial_backbone.fi >> initial_backbone.se;
+    for (int i = 0; i < h; i++) {
+        cin >> layout[i];
+    }
+}
+
+void validate() {
+    int n = backbone_ans.size();
+    int m = router_ans.size();
+    set<pii> uniq_b(backbone_ans.begin(), backbone_ans.end());
+    set<pii> uniq_r(router_ans.begin(), router_ans.end());
+    assert(n == uniq_b.size() && "backbones should be unique");
+    assert(m == uniq_r.size() && "routers should be unique");
+    for (auto p : backbone_ans) {
+        assert(p.fi >= 0 && p.fi < h && "backbones coords must be in range");
+        assert(p.se >= 0 && p.se < w && "backbones coords must be in range");
+    }
+    for (auto p : router_ans) {
+        assert(p.fi >= 0 && p.fi < h && "router coords must be in range");
+        assert(p.se >= 0 && p.se < w && "router coords must be in range");
+    }
 }
 
 void write_result()
